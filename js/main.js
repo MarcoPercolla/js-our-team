757faede6a7632c8007d7268847bd197d0f79a2d
ReDMallard <marco@titori.it>
2023-11-12 19:27:21 +0100

    creo carte(errore tutte uguali)

--- js/main.js
+++ js/main.js
@@ -2,37 +2,37 @@ const membri = [
     {
         nome: "Wayne Barnett" ,
         ruolo: "Founder & CEO" ,
-        imgProfilo:  "wayne-barnett-founder-ceo.jpg",
+        imgProfilo:  "./img/wayne-barnett-founder-ceo.jpg",
 
     },
     {
         nome: "Angela Caroll" ,
         ruolo: "Chief Editor" ,
-        imgProfilo:  "angela-caroll-chief-editor.jpg",
+        imgProfilo:  "./img/angela-caroll-chief-editor.jpg",
 
     },
     {
         nome: "Walter Gordon" ,
         ruolo: "Office Manager" ,
-        imgProfilo:  "walter-gordon-office-manager.jpg",
+        imgProfilo:  "./img/walter-gordon-office-manager.jpg",
 
     },
     {
         nome: "Angela Lopez" ,
         ruolo: "Social Media Manager" ,
-        imgProfilo:  "angela-lopez-social-media-manager.jpg",
+        imgProfilo:  "./img/angela-lopez-social-media-manager.jpg",
 
     },
     {
         nome: "Scott Estrada" ,
         ruolo: "Developer" ,
-        imgProfilo:  "scott-estrada-developer.jpg",
+        imgProfilo:  "./img/scott-estrada-developer.jpg",
 
     },
     {
         nome: "Barbara Ramos" ,
         ruolo: "Graphic Designer" ,
-        imgProfilo:  "barbara-ramos-graphic-designer.jpg",
+        imgProfilo:  "./img/barbara-ramos-graphic-designer.jpg",
 
     },
 ];
@@ -42,7 +42,7 @@ const membri = [
 // console.log(membri[5].imgProfilo);
 
 for (let i = 0; i < membri.length; i++) {
-    const member = membri[i];
+    var member = membri[i];
     // per ogni membro genero una carta
     document.getElementById("container").innerHTML += `<div class="card"> card </div>`;
 
@@ -60,3 +60,15 @@ for (let i = 0; i < membri.length; i++) {
     // };
     
 };
+
+const cards = document.querySelectorAll(".card");
+for (i = 0; i < cards.length; i++) {
+    
+    let immagine = document.createElement("img");
+    immagine.src = member.imgProfilo;
+    cards[i].appendChild(immagine);
+
+    
+    cards[i].innerHTML += `<h3>${member.nome}</h3>`
+    cards[i].innerHTML += `<p>${member.ruolo}</p>`
+}
