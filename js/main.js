55966808f3509118ecd38ebd797472d8fa38d4ab
ReDMallard <marco@titori.it>
2023-11-12 18:49:36 +0100

    aggiunti tutti i membri

--- js/main.js
+++ js/main.js
@@ -5,7 +5,39 @@ const membri = [
         imgProfilo:  "wayne-barnett-founder-ceo.jpg",
 
     },
-    
-]
+    {
+        nome: "Angela Caroll" ,
+        ruolo: "Chief Editor" ,
+        imgProfilo:  "angela-caroll-chief-editor.jpg",
 
-console.log(membri[0].nome)
+    },
+    {
+        nome: "Walter Gordon" ,
+        ruolo: "Office Manager" ,
+        imgProfilo:  "walter-gordon-office-manager.jpg",
+
+    },
+    {
+        nome: "Angela Lopez" ,
+        ruolo: "Social Media Manager" ,
+        imgProfilo:  "angela-lopez-social-media-manager.jpg",
+
+    },
+    {
+        nome: "Scott Estrada" ,
+        ruolo: "Developer" ,
+        imgProfilo:  "scott-estrada-developer.jpg",
+
+    },
+    {
+        nome: "Barbara Ramos" ,
+        ruolo: "Graphic Designer" ,
+        imgProfilo:  "barbara-ramos-graphic-designer.jpg",
+
+    },
+];
+
+console.log(membri[0].nome);
+console.log(membri[3].ruolo);
+console.log(membri[5].imgProfilo);
+
