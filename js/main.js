5d8f0c9c647007b6e3fe039eca86ad94c7b23d67
ReDMallard <marco@titori.it>
2023-11-12 18:45:30 +0100

    aggiunto primo membro js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("ciao");
+const membri = [
+    {
+        nome: "Wayne Barnett" ,
+        ruolo: "Founder & CEO" ,
+        imgProfilo:  "wayne-barnett-founder-ceo.jpg",
+
+    },
+    
+]
+
+console.log(membri[0].nome)
